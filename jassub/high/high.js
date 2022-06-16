7f839980bdd9750f6e837408e9e8e953a7f51e49
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2022-06-16 22:39:02 +0200

    test

--- jassub/high/high.js
+++ jassub/high/high.js
@@ -1,6 +1,6 @@
 const renderer = new JASSUB({
   video: document.querySelector('video'),
-  subUrl: '../../subtitles/box.ass',
+  subUrl: '../subtitles/box.ass',
   workerUrl: '../jassub-worker.js',
   fallbackFont: '../../fonts/default.woff2'
 })
